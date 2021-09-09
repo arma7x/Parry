6aca78045a5ab59fd5fdbccbb6aac25d00144f66
arma7x <arma7x@live.com>
2021-09-09 23:33:13 +0800

    fi

--- app/Views/widgets/commons/footer.php
+++ app/Views/widgets/commons/footer.php
@@ -1,5 +1,3 @@
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-        <span class="text-muted">Page rendered in {elapsed_time} seconds. Environment: <?= ENVIRONMENT ?>. &copy; <?= date('Y') ?></span>
-      </div>
+    <footer class="mt-auto text-white-50">
+      <p>Page rendered in {elapsed_time} seconds. Environment: <?= ENVIRONMENT ?>. &copy; <?= date('Y') ?></p>
     </footer>
--- app/Views/widgets/templates/dashboard_template.php
+++ app/Views/widgets/templates/dashboard_template.php
@@ -3,10 +3,14 @@
   <head>
     <?= view('widgets/commons/header', $this->data); ?>
   </head>
-  <body class="d-flex flex-column h-100">
-    <?= view('widgets/public/navbar', $this->data); ?>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <?= view('widgets/commons/navbar', $this->data); ?>
     <!-- Begin page __content__ -->
+    <main class="px-3">
     <?= isset($__content__) ? $__content__ : ''; ?>
+    </main>
     <?= view('widgets/commons/footer', $this->data); ?>
+    </div>
   </body>
 </html>
