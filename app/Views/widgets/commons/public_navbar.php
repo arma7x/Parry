f7c046db785577c3a741148eed2f009c139ad6bc
arma7x <arma7x@live.com>
2021-09-14 17:49:48 +0800

    show login modal while doing login

--- app/Views/widgets/commons/public_navbar.php
+++ app/Views/widgets/commons/public_navbar.php
@@ -2,9 +2,10 @@
     <div>
       <h3 class="float-md-start mb-0">Parry</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a id="firebaseUserName" class="nav-link d-none" aria-current="page" href="#"></a>
         <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a id="loginButton" class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a>
-        <a id="logoutButton" class="nav-link d-none" href="#" onclick="logout(this);">Logout</a>
+        <a id="firebaseLoginBtn" class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a>
+        <a id="firebaseLogoutBtn" class="nav-link d-none" href="#" onclick="logout(this);">Logout</a>
       </nav>
     </div>
     </header>
