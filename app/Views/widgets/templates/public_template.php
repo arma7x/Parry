4e1747af586faa6bf4d74ae8999db55ccf261a20
arma7x <arma7x@live.com>
2022-01-05 15:26:18 +0800

    move firebase css & script to top

--- app/Views/widgets/templates/public_template.php
+++ app/Views/widgets/templates/public_template.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en" class="h-100">
   <head>
-    <?= view('widgets/commons/header', $this->data); ?>
     <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/5.0.0/firebase-ui-auth.css">
     <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-auth.js"></script>
     <script src="https://www.gstatic.com/firebasejs/ui/5.0.0/firebase-ui-auth.js"></script>
+    <?= view('widgets/commons/header', $this->data); ?>
   </head>
   <body class="d-flex h-100 text-center text-white bg-dark">
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
