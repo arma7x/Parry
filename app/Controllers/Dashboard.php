49d2a7ca71cd80b5fe769e1d2ce10ff376d00673
arma7x <arma7x@live.com>
2022-01-15 00:19:25 +0800

    housekeeping

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,36 +6,14 @@ class Dashboard extends Base\DashboardController
 {
 	public function index()
 	{
-		try {
-			//var_dump($authenticator->getAllUsers([], 10, 20));
-			//var_dump($authenticator->getAllUsers(['id' => 1], 10, 20));
-			//var_dump($user);
-			//$data = [
-				//'username' => strtolower('test'),
-				//'email' => strtolower('test@email.com'),
-				//'password' => '1234567890',
-				//'level' => 255,
-				//'status' => 1,
-				//'create_permission' => 0,
-				//'read_permission' => 0,
-				//'update_permission' => 0,
-				//'delete_permission' => 0
-			//];
-			//var_dump($authenticator->addUser($data));
-			//foreach(['create_permission', 'read_permission', 'update_permission', 'delete_permission'] as $type) {
-				//var_dump($authenticator->hasPermission($user['id'], $type, 1));
-			//}
-			//var_dump();
-			//var_dump($this->user);
-		} catch(\Exception $e) {
-			//var_dump($e->getMessage());
-		}
 		$menus = [];
 		if ($this->user) {
-			$menus = [
-				['text' => 'Manage Internal Users', 'href' => '/internal-users'],
-				['text' => 'Manage Firebase Users', 'href' => '/firebase-users'],
-			];
+			if ((int) $this->user['level'] <= 0){
+				$menus = [
+					['text' => 'Manage Internal Users', 'href' => '/internal-users'],
+					['text' => 'Manage Firebase Users', 'href' => '/firebase-users'],
+				];
+			}
 		}
 		$this->render(['dashboard/main/index'], ['namespace' => get_parent_class($this), 'menus' => $menus]);
 	}
