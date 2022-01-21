ee6da1ede41c2bf7668a1af200581dd2c50c3a75
arma7x <arma7x@live.com>
2022-01-22 04:03:30 +0800

    In progress:manage firebase users

--- app/Controllers/Dashboards/FirebaseUsers.php
+++ app/Controllers/Dashboards/FirebaseUsers.php
@@ -1,11 +1,23 @@
 <?php
 
 namespace App\Controllers\Dashboards;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
+use Kreait\Firebase\Util\JSON;
 
 class FirebaseUsers extends \App\Controllers\Base\DashboardController
 {
 	public function index()
 	{
+		//$firebase = (new Factory)->withServiceAccount(urldecode(env("FIREBASE_CREDENTIALS")));
+		//$httpClient = $firebase->createApiClient([
+		//	'base_uri' => 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/',
+		//]);
+		//$authApiClient = new Auth\ApiClient($httpClient);
+		//$response = $authApiClient->downloadAccount(10, null);
+		//$json = JSON::decode((string) $response->getBody(), true);
+		//var_dump($json);
+		//die;
 		return $this->render(['dashboard/main/index'], ['namespace' => __FUNCTION__]);
 		return $this->response->setStatusCode(200)->setJSON(['message' => __FUNCTION__]);
 	}
